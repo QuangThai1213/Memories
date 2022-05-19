3edc7a18e8e23361cb212ddc774358a62d780290
ThaiTQ <thaimeo0210@gmail.com>
2022-05-19 15:10:45 +0700

    update deploy code

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,21 +2,36 @@ require('dotenv').config()
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId } = require('./config.json');
+const { clientId, guildIds } = require('./config.json');
+const { Client, Intents } = require('discord.js');
 const token = process.env.MY_API_KEY;
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+    ]
+});
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login(token);
 
 const commands = []
 fs.readdirSync('./commands/').forEach(dirs => {
     const lstCommands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
     for (const file of lstCommands) {
         const command = require(`./commands/${dirs}/${file}`);
-        console.log(`${command.data.name} added to slash Command!`);
+        // console.log(`${command.data.name} added to slash Command!`);
         commands.push(command.data.toJSON());
     };
 });
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then(() => console.log('Successfully registered application commands.'))
-    .catch(console.error);
+guildIds.forEach(guildId => {
+    client.fetchGuildPreview(guildId).then(guild => {
+        rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+            .then(() => console.log('Successfully registered application commands to ' + guild.name))
+            .catch(console.error);
+    }).finally(() => {
+        client.destroy();
+    });
+})
